20158105e9715c6918c1a4810a2e678fd4b36393
Gly <gabin.lefranc@gmail.com>
2026-01-07 14:25:10 +0100

    add json5 format

--- src/fs/format/mod.rs
+++ src/fs/format/mod.rs
@@ -1,11 +1,12 @@
 use std::path::Path;
 
-use serde::{Serialize, de::DeserializeOwned};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
 #[derive(Copy, Clone, Debug)]
 pub enum Format {
     Toml,
     Json,
+    Json5,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -18,6 +19,8 @@ pub enum Error {
     TomlSerialize(#[from] toml::ser::Error),
     #[error("json serialize/deserialize error: {0}")]
     Json(#[from] serde_json::Error),
+    #[error("json5 serialize/deserialize error: {0}")]
+    Json5(#[from] json5::Error),
 }
 
 impl Format {
@@ -25,6 +28,7 @@ impl Format {
         match self {
             Format::Toml => "toml",
             Format::Json => "json",
+            Format::Json5 => "json5",
         }
     }
     pub fn write_file<Data: Serialize, P: AsRef<Path>>(
@@ -33,28 +37,31 @@ impl Format {
         data: Data,
     ) -> Result<(), Error> {
         use std::io::Write;
+        let mut file = std::fs::File::create(path)?;
         match self {
             Format::Toml => {
                 let toml_data = toml::to_string_pretty(&data)?;
-                let mut file = std::fs::File::create(path)?;
                 file.write_all(toml_data.as_bytes())?;
             }
-            Format::Json => {
-                let file = std::fs::File::create(path)?;
-                serde_json::to_writer(file, &data)?;
-            }
+            Format::Json => serde_json::to_writer(file, &data)?,
+            Format::Json5 => json5::to_writer(file, &data)?,
         }
         Ok(())
     }
     pub fn read_file<Data: DeserializeOwned, P: AsRef<Path>>(self, path: P) -> Result<Data, Error> {
+        use std::io::Read;
+        let mut file = std::fs::File::open(path)?;
         match self {
             Format::Toml => {
-                let content = std::fs::read_to_string(path)?;
+                let mut content = String::new();
+                file.read_to_string(&mut content)?;
                 Ok(toml::from_str(&content)?)
             }
-            Format::Json => {
-                let file = std::fs::File::open(path)?;
-                Ok(serde_json::from_reader(file)?)
+            Format::Json => Ok(serde_json::from_reader(file)?),
+            Format::Json5 => {
+                let mut content = String::new();
+                file.read_to_string(&mut content)?;
+                Ok(json5::from_str(&content)?)
             }
         }
     }
